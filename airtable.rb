1b74f64cd024116b5772ca105108a8629e70d06e
24c02 <163450896+24c02@users.noreply.github.com>
2025-01-29 20:45:09 -0500

    make things configurable!

--- airtable.rb
+++ airtable.rb
@@ -26,20 +26,39 @@ class YSWSProgram < Logo
 end
 
 class CommunityLogo < Logo
-  N = 15
-
   self.base_key = "appOOH0x03NEHw8Rt"
   self.table_name = 'tbl1blRyjt0AUVBvJ'
 
   class << self
     def pick_logo
+      if Config.get['override']
+        logo = find Config.get['override'][0]
+        puts "overridden to #{logo['Name']}."
+        return logo
+      end
       puts "sampling logos..."
-      candidates = records(filter: 'AND({Active}, {Slack Icon})', sort: {"Last Shown" => "asc"}, max_records: N)
+      candidates = records(filter: 'AND({Active}, {Slack Icon})', sort: {"Last Shown" => "asc"}, max_records: Config.get['randomize_count'])
       logo = candidates.sample
       raise "what?!" unless logo
       logo.patch('Last Shown' => Time.now)
       puts "picked #{logo['Name']}!"
       logo
+    end
+  end
+end
+
+class Config < Norairrecord::Table
+  self.base_key = "appOOH0x03NEHw8Rt"
+  self.table_name = "tblN8zhtIgHBQLHBm"
+  class << self
+    def fetch
+      find "recgEG7YTjRcKI1jq?"
+    end
+    def get
+      @config ||= fetch
+    end
+    def refresh
+      @config = fetch
     end
   end
 end
